0188e3f229c5939ecbb9d0cc786f50e0da7fe07d
0Xiaohei0 <15852786007ysc@gmail.com>
2022-08-28 17:10:13 -0400

    Added hero

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,7 +3,12 @@ import React from "react"
 export default function Hero(){
     return(
         <div className="hero--container">
-            
+            <div className="hero--textContainer">
+                <p className="hero--name">Sicheng Yi</p>
+                <h1 className="hero--role">Web and game developer</h1>
+                <p className="hero--description">I'm a mathematic student at the University of Waterloo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis libero ac est rhoncus, a condimentum.</p>
+                <p className="hero--viewProjects">View Projects</p>
+            </div>
         </div>
     )
 }
