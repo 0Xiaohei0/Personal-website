f51c241ee6b3b24dfcf699925097e1ce9bbe7cc1
0Xiaohei0 <15852786007ysc@gmail.com>
2022-08-28 16:59:33 -0400

    Added navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,18 @@ import React from "react"
 
 export default function Navbar(){
     return(
-        <h1>Navbar</h1>
+        <nav className="navbar">
+            <div className="navbar--nameContainer">
+                <p className="navbar--name">Sicheng Yi</p>
+                <p className="navbar--role">Web and game developer</p>
+            </div>
+            <div className="navbar--linkContainer">
+                <p className="navbar--link">About</p>
+                <p className="navbar--link">Skills</p>
+                <p className="navbar--link">Experience</p>
+                <p className="navbar--link">Projects</p>
+                <p className="navbar--link">Contact</p>
+            </div>
+        </nav>
     )
 }
