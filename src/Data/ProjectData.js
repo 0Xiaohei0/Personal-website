b16bbd1442f592f3a6e8bdceae6863604d090ae0
0Xiaohei0 <15852786007ysc@gmail.com>
2022-08-29 19:35:08 -0400

    Added more project data

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,26 +1,43 @@
 import FIS2Cover from "../images/FIS2.png";
 import BIQCover from "../images/biq.png";
+import EventCover from "../images/Event.png";
+import FIS1Cover from "../images/FIS1.jpg";
+import TimeCover from "../images/Time.png";
 
 const ProjectData = [
   {
     key: 0,
     imageUrl: FIS2Cover,
-    description: `• A 2d platformer game built in a two-person team using C# in the Unity game engine.`,
-    name: "Fortress in the sky",
+    name: "Fortress in the sky 2",
+    description: `• A 3d platformer game with self made 3D assets, built with the Unity game engine.`,
     projectUrl: "https://github.com/0Xiaohei0/ConnectedWorlds",
   },
   {
     key: 1,
     imageUrl: BIQCover,
-    description: `• Local multiplayer tetris game build with c++ in a three person team.`,
     name: "Biquadris",
+    description: `• Local multiplayer tetris game build with c++ in a three person team.`,
     projectUrl: "https://github.com/0Xiaohei0/ConnectedWorlds",
   },
   {
     key: 2,
-    imageUrl: "FIS2.png",
-    description: `• A web application that allows its users to post and attend events.`,
+    imageUrl: EventCover,
     name: "Event link",
+    description: `• A web application that allows its users to post and attend events.`,
+    projectUrl: "https://github.com/0Xiaohei0/ConnectedWorlds",
+  },
+  {
+    key: 3,
+    imageUrl: FIS1Cover,
+    name: "Fortress in the sky",
+    description: `• A 2d platformer game built in a two-person team using C# in the Unity game engine.`,
+    projectUrl: "https://github.com/0Xiaohei0/ConnectedWorlds",
+  },
+  {
+    key: 4,
+    imageUrl: TimeCover,
+    name: "Timelapse generator",
+    description: `• Python automation script for converting video files into timelapse.`,
     projectUrl: "https://github.com/0Xiaohei0/ConnectedWorlds",
   },
 ];
