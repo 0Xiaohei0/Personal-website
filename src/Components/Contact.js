fc72d6fdf66c3832954ab1eb848b50ab167bfb76
0Xiaohei0 <15852786007ysc@gmail.com>
2022-09-08 10:41:18 -0400

    fix warnings

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,18 +1,30 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import Github from "../images/github.png";
+import Email from "../images/envelope-solid.png";
+import Itch from "../images/itch-io.png";
+import Link from "../images/linkedin.png";
 
 export default function Contact() {
   return (
     <div className="Contact" id="Contact">
-      <a className="Contact--item">
-        <FontAwesomeIcon icon={faEnvelope} />
+      <a href="https://github.com/0Xiaohei0" className="Contact--item">
+        <img alt="externalLinks" className="Contact--icon" src={Github} />
       </a>
-      <a className="Contact--item">
-        <FontAwesomeIcon icon={faEnvelope} />
+      <a href="mailto:s35yi@uwaterloo.ca" className="Contact--item">
+        <img alt="externalLinks" className="Contact--icon" src={Email} />
       </a>
-      <a className="Contact--item">
-        <FontAwesomeIcon icon={faEnvelope} />
+      <div className="Contact--nameContainer">
+        <p className="navbar--name">Sicheng Yi</p>
+        <p className="navbar--role">Web and game developer</p>
+      </div>
+      <a
+        href="https://www.linkedin.com/in/sicheng-yi-6a8256212/"
+        className="Contact--item"
+      >
+        <img alt="externalLinks" className="Contact--icon" src={Link} />
+      </a>
+      <a href="https://daydreamstudio.itch.io/" className="Contact--item">
+        <img alt="externalLinks" className="Contact--icon" src={Itch} />
       </a>
     </div>
   );
